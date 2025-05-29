3a2cfb5bc5501971707e0553361607949893b550
meriam <meriam.hamdaoui@gmail.com>
2025-05-29 08:41:20 +0100

    todo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-
+import TodoForm from "./components/TodoForm/TodoForm";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img className={styles.Logo} src="/public/img/to-do-list.png" />
+        <img className={styles.Logo} src="/img/to-do-list.png" />
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
 
-      <div className={styles.AppContainer}>Content</div>
+      <div className={styles.AppContainer}>
+        <TodoForm />
+      </div>
     </div>
   );
 }
