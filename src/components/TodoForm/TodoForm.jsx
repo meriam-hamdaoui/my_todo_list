f8eb3023f773f89d0dad979aad8cb0ee941bf7bb
meriam <meriam.hamdaoui@gmail.com>
2025-05-30 11:20:21 +0100

    extract the form fields component

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Todo.module.css";
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/data.js";
+import TodoFormFields from "../TodoFormFields/TodoFormFields.jsx";
 
 const TodoForm = ({ onCreate }) => {
   const [showField, setShowField] = useState(false);
@@ -34,52 +35,7 @@ const TodoForm = ({ onCreate }) => {
       </h3>
 
       <form className={styles.Form} onSubmit={handleSubmit}>
-        <div className={styles.FormFields}>
-          <div className={styles.FormField}>
-            <input
-              type="text"
-              aria-label="Name*"
-              placeholder="Name*"
-              name="name"
-              autoComplete="off"
-            />
-          </div>
-          {showField && (
-            <>
-              {" "}
-              <div className={styles.FormField}>
-                <textarea
-                  aria-label="Description"
-                  placeholder="Description"
-                  name="description"
-                  rows="3"
-                />
-              </div>
-              <div className={styles.FormGroup}>
-                <div className={styles.FormField}>
-                  <label htmlFor="deadline">Deadline</label>
-                  <input type="date" id="deadline" name="deadline" />
-                </div>
-
-                <div className={styles.FormField}>
-                  <label htmlFor="priority">Priority</label>
-                  <select
-                    defaultValue={PRIORITY_DEFAULT}
-                    id="priority"
-                    name="priority"
-                  >
-                    {Object.entries(PRIORITIES).map(([key, { label }]) => (
-                      <option key={key} value={key}>
-                        {label}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-
+        <TodoFormFields showField={showField} />
         <input type="submit" value="Add" />
       </form>
     </section>
---
+++ src/components/TodoFormFields/TodoFormFields.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/data.js";
+import styles from "./TodoFormFields.module.css";
+
+const TodoFormFields = ({ showField = true }) => {
+  return (
+    <div className={styles.FormFields}>
+      <div className={styles.FormField}>
+        <input
+          type="text"
+          aria-label="Name*"
+          placeholder="Name*"
+          name="name"
+          autoComplete="off"
+        />
+      </div>
+      {showField && (
+        <>
+          {" "}
+          <div className={styles.FormField}>
+            <textarea
+              aria-label="Description"
+              placeholder="Description"
+              name="description"
+              rows="3"
+            />
+          </div>
+          <div className={styles.FormGroup}>
+            <div className={styles.FormField}>
+              <label htmlFor="deadline">Deadline</label>
+              <input type="date" id="deadline" name="deadline" />
+            </div>
+
+            <div className={styles.FormField}>
+              <label htmlFor="priority">Priority</label>
+              <select
+                defaultValue={PRIORITY_DEFAULT}
+                id="priority"
+                name="priority"
+              >
+                {Object.entries(PRIORITIES).map(([key, { label }]) => (
+                  <option key={key} value={key}>
+                    {label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TodoFormFields;
