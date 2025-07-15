049eca3acd53971070cc7987d1d02e8418166755
meriam <meriam.hamdaoui@gmail.com>
2025-07-15 19:06:50 +0100

    adjust the empty todo tab name

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,8 @@ import TodoItem from "../TodoItem/TodoItem";
 const TodoList = ({ todoList, onUpdate, onDelete }) => {
   return (
     <div>
-      <h3 align="center">To-Do's</h3>
+      <h3 align="center">{todoList.length ? "To-Do's" : "Nothing To Do"}</h3>
+
       <ul className={styles.TodoList}>
         {todoList.map((todo) => (
           <TodoItem
